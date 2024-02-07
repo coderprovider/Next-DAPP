9988f669dfd49ab1bc332a55bb951e413a8c3033
whitefury01 <white.pury1114@gmail.com>
2024-02-07 09:55:09 +0900

    create_footer_com

---
+++ next-auth-app/app/components/footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer : React.FC = () => {
+
+    return (
+        <div className="h-14 bg-emerald-300 p-3 flex items-center">
+            Next Auth
+        </div>
+    )
+}
+
+export default Footer;
